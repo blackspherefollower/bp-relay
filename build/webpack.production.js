097176357609d6b076652383f1ec9f7c8816871c
blackspherefollower <darktech@black-sphere.co.uk>
2019-06-05 12:13:29 +0100

    fix: Removing Uglyfi to fix builds

--- build/webpack.production.js
+++ build/webpack.production.js
@@ -2,7 +2,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.base.js');
 const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "production",
@@ -12,21 +11,6 @@ module.exports = merge(common, {
     minimize: false,
   },
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true,
-      parallel: true,
-      cache: true,
-      uglifyOptions: {
-        mangle: {
-          keep_classnames: true,
-          keep_fnames: true
-        },
-        compress: {
-          keep_fnames: true,
-          keep_classnames: true,
-        }
-      }
-    }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
